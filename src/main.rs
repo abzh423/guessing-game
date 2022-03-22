8c3280d3c2f0bc8a85fa30a85b5be5f72757ffab
Abulkhaiyr <abzh423@protonmail.com>
2022-03-22 18:54:10 +0600

    Final commit, the project is done

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
+#![warn(clippy::all, clippy::pedantic)]
+
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number.");
+
+    let secret_number: u8 = rand::thread_rng().gen_range(1..101);
+
+    println!("Please enter an input.");
+
+    loop {
+        let mut guess = String::new();
+        io::stdin().read_line(&mut guess).expect("Unknown input.");
+
+        let guess: u8 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("You guessed {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Equal => {
+                println!("Bingo!");
+                break;
+            }
+            Ordering::Greater => println!("Too large!"),
+        }
+    }
 }
